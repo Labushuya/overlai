adbf09b136f673eb9103d36422c6fa1180de86fd
OverlAI <overlai@local>
2026-07-31 12:41:04 +0200

    fix(core-ui): getValue-Delegate-Import für by rememberUpdatedState in OnResume

--- core/core-ui/src/main/kotlin/de/overlai/core/ui/util/OnResume.kt
+++ core/core-ui/src/main/kotlin/de/overlai/core/ui/util/OnResume.kt
@@ -2,6 +2,7 @@ package de.overlai.core.ui.util
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
